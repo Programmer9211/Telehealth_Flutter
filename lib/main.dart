271052bf425e05d55d3fd440e0d36045db465c5b
Programmer9211 <adityachaudhary1029@gmail.com>
2021-04-29 19:36:31 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:tele_health_app/Authenticate/LoginScreen.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: LoginScreen(),
+    );
+  }
+}
